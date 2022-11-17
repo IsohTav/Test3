21ebf49a7de44434316ee5116fc6e60b5bcfece8
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-17 17:56:07 +1100

    Update talking.js

--- talking.js
+++ talking.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 
-router.get(‘/handle’,(request,response => {
+router.get(‘/handle’,(req, res) => {
     
 });
 
