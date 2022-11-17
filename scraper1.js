48ba5c23dc20aab5ebb0345369344f1852a97c38
IsohTav <118420382+IsohTav@users.noreply.github.com>
2022-11-17 17:33:13 +1100

    Update scraper1.js

--- scraper1.js
+++ scraper1.js
@@ -1,16 +1,22 @@
 const puppeteer = require('puppeteer');
 
+
+app.post('/test1', function(req,res){
+	return res.redirect('https://www.youtube.com/watch?v=0tOgMSEPFRs');
+
+});
+
 async function scrapeItem(url) {
 	
-	const browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
+	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(url);
 
 	const [el] = await page.$x('/html/body/section[2]/div/div[1]/div[1]/dl/dd/span');
-	const text = await el.getProperty('textContent')
+	const text = await el.getProperty('p')
 	const textTXT = await text.jsonValue();
 
-	console.log({textTXT});
+	cnsole.log({textTXT});
 }
 
 scrapeItem('https://www.onlinejobs.ph/jobseekers/info/248015')
